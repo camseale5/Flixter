77c22b18f797c242dcf2e5d347794c995404f46d
Mark Seale <camseale5@yahoo.com>
2020-01-17 01:39:19 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :user_enrollment_status, only: [:show]
+  
   def show
   end
 
-  private
+
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+  
+  private
+  
+  def user_enrollment_status
+  if current_lesson.section.course.user != current_user
+      redirect_to root_url, alert: 'Error Message Here'
+  end
+end
 end
